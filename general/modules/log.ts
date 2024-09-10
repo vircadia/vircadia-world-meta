aef7ae9e7912580efd7112d3615b8c698776799b
Kalila <69767640+digisomni@users.noreply.github.com>
2024-09-10 23:45:33 +1000

    Update logging.

--- general/modules/log.ts
+++ general/modules/log.ts
@@ -1,22 +1,32 @@
 import { log as colorLog } from 'npm:console-log-colors';
 
-export function log(
+export function log(data: {
     message: string,
-    type: 'info' | 'success' | 'error' | 'warning' | 'warn' = 'info',
-): void {
-    switch (type) {
+    type?: 'info' | 'success' | 'error' | 'warning' | 'warn' | 'debug',
+    debug?: boolean,
+}): void {
+    if (!data.type) {
+        data.type = 'info';
+    }
+
+    switch (data.type) {
+        case 'debug':
+            if (data.debug) {
+                colorLog.blue('ℹ', data.message);
+            }
+            break;
         case 'info':
-            colorLog.blue('ℹ', message);
+            colorLog.blue('ℹ', data.message);
             break;
         case 'success':
-            colorLog.green('✔', message);
+            colorLog.green('✔', data.message);
             break;
         case 'error':
-            colorLog.red('✖', message);
+            colorLog.red('✖', data.message);
             break;
         case 'warn':
         case 'warning':
-            colorLog.yellow('⚠', message);
+            colorLog.yellow('⚠', data.message);
             break;
     }
 }
